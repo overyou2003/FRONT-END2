b57e73d1d5515a00b72b7b13c2de3b20f02a72f7
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2024-02-13 08:51:32 +0700

    dsada

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -5,7 +5,8 @@ const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
 
-const base_url = "http://localhost:3000";
+//const base_url = "http://localhost:3000";
+const base_url = "http://node57391-patiphat-noderest.proen.app.ruk-com.cloud";
 
 app.set('view engine' , 'ejs');
 app.use(bodyParser.json());
