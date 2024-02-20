92c099e4845ba7ccc49f2f5dcf2ec87df7968d4b
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2024-02-20 11:01:12 +0700

    dsa

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -6,8 +6,8 @@ var bodyParser = require('body-parser');
 
 
 
-const base_url = "http://node57391-patiphat-noderest.proen.app.ruk-com.cloud"; 
-//const base_url = "http://localhost:3000";
+//const base_url = "http://node57391-patiphat-noderest.proen.app.ruk-com.cloud"; 
+const base_url = "http://localhost:3000";
 
 app.set("views", path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs');
